e115a7d78e4fc64840fb0ac68b28296b8e05b60b
Erenalp0589 <64745658+Erenalp0589@users.noreply.github.com>
2020-11-11 11:03:50 +0300

    Add files via upload

---
+++ WebService/models/user.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+import db from "../db";
+
+const SALT_ROUNDS = 7;
+
+const hashPassword = (password) => bcrypt.hash(password, SALT_ROUNDS);
+const verifyPassword = (password, hash) => bcrypt.compare(password, hash);
+
+const checkAndHash = (user) => {
+  if (!user.password) return Promise.resolve(user);
+
+  return hashPassword(user.password)
+    .then((hash) => {
+      user.password = hash;
+    })
+    .catch((err) => `Error Hashing: ${err}`);
+};
+
+const bookshelf = require("bookshelf")(db);
+
+const User = bookshelf.model(
+  "User",
+  {
+    initialize() {
+      this.constructor.__super__.initialize.apply(this, arguments);
+
+      this.on("creating", this.validateSave);
+      this.on("updating", this.validateUpdate);
+    },
+    tableName: "user",
+    validateUpdate(model, attrs) {
+      if (model.password !== attrs) return checkAndHash(this.attributes);
+      return this.attributes;
+    },
+    validateSave() {
+      return checkAndHash(this.attributes);
+    },
+  },
+  {
+    
+    verify(password, data) {
+      return verifyPassword(password, data);
+    },
+  }
+);
+
+export default User;
