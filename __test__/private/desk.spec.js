5fb29ff371d22ac12ddb09089188b1eb133baa4f
Erenalp0589 <64745658+Erenalp0589@users.noreply.github.com>
2020-11-17 01:54:05 +0300

    test cases

---
+++ __test__/private/desk.spec.js
@@ -0,0 +1,409 @@
+// Test Requirements
+import request from 'supertest';
+import express from 'express';
+import moment from 'moment';
+import bodyParser from 'body-parser';
+
+// Models
+import AverageTimeModel from '../../models/averageTime';
+import DeskModel from '../../models/desk';
+import DeskUserModel from '../../models/deskUser';
+
+import server from '../../services/desk_service';
+import { Model } from 'bookshelf';
+const app = express();
+app.use(bodyParser.json());
+app.use(server);
+
+describe('Desk Endpoints', () => {
+  describe('Create Desk', () => {
+    test('should be exists route', (done) => {
+      request(app)
+        .post('/desk/')
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+    test('should name be defined', (done) => {
+      request(app)
+        .post('/desk/')
+        .send({
+          name: '',
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('Name must be exists');
+          done();
+        });
+    });
+    test('should create desk', (done) => {
+      request(app)
+        .post('/desk/')
+        .send({
+          name: 'test',
+        })
+        .expect(201)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.id).toBeTruthy();
+          expect(body.name).toBe('test');
+
+          DeskModel.where({ id: body.id })
+            .fetch()
+            .then((savedDesk) => {
+              expect(savedDesk.get('name')).toBe(body.name);
+              expect(savedDesk.get('is_active')).toBeTruthy();
+              expect(savedDesk.get('is_deleted')).toBeFalsy();
+              done();
+            });
+        });
+    });
+  });
+  describe('Remove Desk', () => {
+    let deskID, removedDeskID;
+    beforeEach(async (done) => {
+      new DeskModel({
+        name: 'test',
+      })
+        .save()
+        .then((savedDesk) => {
+          deskID = savedDesk.id;
+
+          new DeskModel({
+            name: 'test',
+            is_deleted: true,
+          })
+            .save()
+            .then((savedDesk) => {
+              removedDeskID = savedDesk.id;
+              done();
+            });
+        });
+    });
+    test('should be exists route', (done) => {
+      request(app)
+        .delete('/desk/1')
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+    test('should be param id as number', (done) => {
+      request(app)
+        .delete('/desk/ads')
+        .send({
+          name: '',
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('id must be number');
+          done();
+        });
+    });
+    test('should not remove non-exist desk', (done) => {
+      request(app)
+        .delete('/desk/' + 10)
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('No Rows Updated');
+          done();
+        });
+    });
+    test('should not remove already removed desk', (done) => {
+      request(app)
+        .delete('/desk/' + removedDeskID)
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('No Rows Updated');
+          done();
+        });
+    });
+    test('should remove desk', (done) => {
+      request(app)
+        .delete('/desk/' + deskID)
+        .expect(200)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.id).toBeTruthy();
+          DeskModel.where({ id: body.id })
+            .fetch()
+            .then((savedDesk) => {
+              expect(savedDesk.get('is_deleted')).toBeTruthy();
+              done();
+            });
+        });
+    });
+  });
+
+  describe('List Desks', () => {
+    beforeEach(async (done) => {
+      /*
+        Creating 6 Desk (Deleted 3)
+      */
+      Promise.all([
+        new DeskModel({
+          name: 'test',
+        }).save(),
+        new DeskModel({
+          name: 'test2',
+          is_deleted: true,
+        }).save(),
+        new DeskModel({
+          name: 'test3',
+        }).save(),
+        new DeskModel({
+          name: 'test4',
+          is_deleted: true,
+        }).save(),
+        new DeskModel({
+          name: 'test5',
+        }).save(),
+        new DeskModel({
+          name: 'test6',
+          is_deleted: true,
+        }).save(),
+      ]).then(() => {
+        done();
+      });
+    });
+    test('should be exists route', (done) => {
+      request(app)
+        .get('/desk')
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+    test('should be list valid desks(non-deleted)', (done) => {
+      request(app)
+        .get('/desk')
+        .expect(200)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.data).toHaveLength(3);
+          done();
+        });
+    });
+  });
+
+  describe('Activate/Deactivate Desk', () => {
+    test('should be exists route', (done) => {
+      request(app)
+        .put('/desk/1')
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+    test('should status exists', (done) => {
+      request(app)
+        .put('/desk/1')
+        .send({})
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeDefined();
+          expect(body.message).toBe('status must be exists');
+          done();
+        });
+    });
+    test('should be param id as number', (done) => {
+      request(app)
+        .put('/desk/ads')
+        .send({
+          name: '',
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('id must be number');
+          done();
+        });
+    });
+    test('should not remove non-exist desk', (done) => {
+      request(app)
+        .put('/desk/' + 10)
+        .send({
+          status: false,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNil();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('No Rows Updated');
+          done();
+        });
+    });
+
+    describe('Setting Status', () => {
+      let activatedDeskID, deactivatedDeskID;
+      beforeEach(async (done) => {
+        new DeskModel({
+          name: 'test',
+          is_active: true,
+        })
+          .save()
+          .then((savedDesk) => {
+            activatedDeskID = savedDesk.id;
+            new DeskModel({
+              name: 'test',
+              is_active: false,
+            })
+              .save()
+              .then((savedDesk) => {
+                deactivatedDeskID = savedDesk.id;
+                done();
+              });
+          });
+      });
+
+      test('should not activate already activated desk', (done) => {
+        request(app)
+          .put('/desk/' + activatedDeskID)
+          .send({
+            status: true,
+          })
+          .expect(400)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.error).toBeTruthy();
+            expect(body.message).toBe('No Rows Updated');
+            done();
+          });
+      });
+
+      test('should activate deactivated desk', (done) => {
+        request(app)
+          .put('/desk/' + deactivatedDeskID)
+          .send({
+            status: true,
+          })
+          .expect(200)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.id).toBeTruthy();
+            DeskModel.where({ id: body.id })
+              .fetch()
+              .then((updatedDesk) => {
+                expect(updatedDesk.get('is_active')).toBeTruthy();
+                done();
+              });
+          });
+      });
+
+      test('should deactivate activated desk', (done) => {
+        request(app)
+          .put('/desk/' + activatedDeskID)
+          .send({
+            status: false,
+          })
+          .expect(200)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.id).toBeTruthy();
+            DeskModel.where({ id: body.id })
+              .fetch()
+              .then((updatedDesk) => {
+                expect(updatedDesk.get('is_active')).toBeFalsy();
+                done();
+              });
+          });
+      });
+
+      test('should not deactivate already deactivated desk', (done) => {
+        request(app)
+          .put('/desk/' + deactivatedDeskID)
+          .send({
+            status: false,
+          })
+          .expect(400)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.error).toBeTruthy();
+            expect(body.message).toBe('No Rows Updated');
+            done();
+          });
+      });
+    });
+  });
+});
+
+describe('Average Time', () => {
+  let deskID;
+
+  beforeEach(async (done) => {
+    new DeskModel({
+      name: 'test',
+    })
+      .save()
+      .then((savedDesk) => {
+        deskID = savedDesk.id;
+        done();
+      });
+  });
+  describe('Set Average Time', () => {
+    test('should add average time', (done) => {
+      request(app)
+        .post('/desk/stats/time')
+        .send({
+          time: 10,
+          deskId: deskID,
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.error, res.body.message).toBeUndefined();
+          expect(err).toBeNil();
+          done();
+        });
+    });
+
+    test('should not add zero or negative average time', (done) => {
+      request(app)
+        .post('/desk/stats/time')
+        .send({
+          time: 0,
+          desk_id: deskID,
+        })
+        .expect(400)
+        .end((err, res) => {
+          expect(res.body.error, res.body.message).toBeTruthy();
+          expect(err).toBeNil();
+          done();
+        });
+    });
+  });
+  describe('Get Average Time', () => {
+    beforeEach(async (done) => {
+      await new AverageTimeModel({ time: 10, desk_id: deskID }).save();
+      await new AverageTimeModel({ time: 20, desk_id: deskID }).save();
+      await new AverageTimeModel({ time: 30, desk_id: deskID }).save();
+
+      done();
+    });
+    test('should add latest average time', (done) => {
+      request(app)
+        .get('/desk/stats/time')
+        .query({
+          deskId: deskID,
+        })
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.error, res.body.message).toBeUndefined();
+          expect(err).toBeNil();
+          expect(res.body.time).toBe(30);
+          expect(res.body.created_at).toBeTruthy();
+          done();
+        });
+    });
+  });
+});
---
+++ __test__/private/queue.spec.js
@@ -0,0 +1,535 @@
+// Test Requirements
+import request from 'supertest';
+import express from 'express';
+import moment from 'moment';
+import bodyParser from 'body-parser';
+
+// Models
+import AverageTimeModel from '../../models/averageTime';
+import DeskModel from '../../models/desk';
+import DeskUserModel from '../../models/deskUser';
+import UserModel from '../../models/user';
+
+import server from '../../services/queue_service';
+import { Model } from 'bookshelf';
+
+describe('Queue Process', () => {
+  let app, userID;
+  beforeAll(async (done) => {
+    userID = await new UserModel({
+      username: 'testxxxx',
+      password: '123456',
+      priority_key: 1,
+    })
+      .save()
+      .then((savedUser) => savedUser.id);
+
+    app = express();
+    app.use(bodyParser.json());
+    // Set default user
+    app.use((req, res, next) => {
+      req.userData = {
+        uid: userID,
+      };
+      next();
+    });
+    app.use(server);
+    done();
+  });
+
+
+
+  describe('Joining to a queue', () => {
+    let deletedDesk, deactivatedDesk, normalDeskID;
+    beforeEach(async () => {
+      normalDeskID = await new DeskModel({
+        name: 'test',
+        is_active: true,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+      deactivatedDesk = await new DeskModel({
+        name: 'test2',
+        is_active: false,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+      deletedDesk = await new DeskModel({
+        name: 'test3',
+        is_active: false,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+    });
+
+    test('should be route exists', (done) => {
+      request(app)
+        .post('/queue/')
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+
+    test('should be desk_id defined', (done) => {
+      request(app)
+        .post('/queue/')
+        .expect(400)
+        .end((err, { body }) => {
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('deskId must be exists');
+          expect(err).toBeNull();
+          done();
+        });
+    });
+
+    test('should be desk exists and active', (done) => {
+      request(app)
+        .post('/queue/')
+        .send({
+          deskId: 999,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNull();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('desk must be valid');
+          done();
+        });
+    });
+
+    test('should be active desk', (done) => {
+      request(app)
+        .post('/queue/')
+        .send({
+          deskId: deactivatedDesk,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNull();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('desk must be valid');
+          done();
+        });
+    });
+
+
+    test('should be not deleted desk', (done) => {
+      request(app)
+        .post('/queue/')
+        .send({
+          deskId: deletedDesk,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNull();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('desk must be valid');
+          done();
+        });
+    });
+    test('should be add into valid desk', async (done) => {
+      let userID;
+      userID = await new UserModel({
+        username: 'testxxxx',
+        password: '123456',
+        priority_key: 1,
+      }).save().then( savedUser => savedUser.id);
+
+      app.use((req, res, next) => {
+        req.userData = {
+            uid: userID,
+          };
+          next();
+        });
+
+      request(app)
+        .post('/queue/')
+        .send({
+          deskId: normalDeskID,
+        })
+        .expect(200)
+        .end((err, { body }) => {
+          expect(err, body.message).toBeNull();
+          expect(body.id).toBeGreaterThan(0);
+          expect(body.desk_id).toBe(normalDeskID);
+          expect(body.user_id).toBe(userID);
+          expect(body.is_active).toBeTruthy();
+          expect(body.is_finished).toBeFalsy();
+          done();
+        });
+    });
+  });
+
+  describe('Giving Up a queue', () => {
+    let deletedDesk, deactivatedDesk, normalDeskID, gaveUpDeskID;
+    beforeEach(async () => {
+      userID = await new UserModel({
+        username: 'testxxxx',
+        password: '123456',
+        priority_key: 1,
+      })
+        .save()
+        .then((savedUser) => savedUser.id);
+  
+      normalDeskID = await new DeskModel({
+        name: 'test',
+        is_active: true,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+      gaveUpDeskID = await new DeskModel({
+        name: 'test',
+        is_active: true,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+      deactivatedDesk = await new DeskModel({
+        name: 'test2',
+        is_active: false,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+      deletedDesk = await new DeskModel({
+        name: 'test3',
+        is_active: false,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+      await new DeskUserModel({
+        desk_id: normalDeskID,
+        user_id: userID,
+      }).save();
+
+      await new DeskUserModel({
+        desk_id: gaveUpDeskID,
+        user_id: userID,
+        is_active: false,
+      }).save();
+    });
+    test('should be route exists', (done) => {
+      request(app)
+        .delete('/queue/')
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+
+    test('should be desk_id defined', (done) => {
+      request(app)
+        .delete('/queue/')
+        .expect(400)
+        .end((err, { body }) => {
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('deskId must be exists');
+          expect(err).toBeNull();
+          done();
+        });
+    });
+
+
+    test('should be desk exists and active', (done) => {
+      request(app)
+        .delete('/queue/')
+        .send({
+          deskId: 999,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNull();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('desk must be valid');
+          done();
+        });
+    });
+    test('should be active desk', (done) => {
+      request(app)
+        .delete('/queue/')
+        .send({
+          deskId: deactivatedDesk,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNull();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('desk must be valid');
+          done();
+        });
+    });
+    test('should be not deleted desk', (done) => {
+      request(app)
+        .delete('/queue/')
+        .send({
+          deskId: deletedDesk,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err).toBeNull();
+          expect(body.error).toBeTruthy();
+          expect(body.message).toBe('desk must be valid');
+          done();
+        });
+    });
+
+    test('should not give up already given up desk', (done) => {
+      request(app)
+        .delete('/queue/')
+        .send({
+          deskId: gaveUpDeskID,
+        })
+        .expect(400)
+        .end((err, { body }) => {
+          expect(err, body.message).toBeNull();
+          expect(body.error).toBeDefined();
+          expect(body.message).toBe('user has not been given up');
+          done();
+        });
+
+
+    });
+    test('should give up from valid desk', (done) => {
+      request(app)
+        .delete('/queue/')
+        .send({
+          deskId: normalDeskID,
+        })
+        .expect(200)
+        .end((err, { body }) => {
+          expect(err, body.message).toBeNull();
+          expect(body.id).toBeGreaterThan(0);
+          expect(body.desk_id).toBe(normalDeskID);
+          expect(body.user_id).toBe(userID);
+          expect(body.is_active).toBeFalsy();
+          expect(body.is_finished).toBeFalsy();
+          done();
+        });
+    });
+  });
+
+  describe('Get Stats', () => {
+    test('should be occupancy route exists', (done) => {
+      request(app)
+        .get('/queue/occupancy/' + 1)
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+    test('should be info route exists', (done) => {
+      request(app)
+        .get('/queue/info/' + 1)
+        .end((err, res) => {
+          expect(res.status).not.toBe(404);
+          done();
+        });
+    });
+    describe('Occupancy', () => {
+      let normalDeskID, userID;
+      beforeEach(async () => {
+        userID = await new UserModel({
+          username: 'testxxxx',
+          password: '123456',
+          priority_key: 1,
+        })
+          .save()
+          .then((savedUser) => savedUser.id);
+    
+        normalDeskID = await new DeskModel({
+          name: 'test',
+          is_active: true,
+          is_deleted: false,
+        }).save().then( (saved) => saved.id);
+
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+          is_active: false,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+          is_finished: true,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+      });
+
+      test('should desk id is number', (done) => {
+        request(app)
+          .get('/queue/occupancy/asd')
+          .expect(400)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.error).toBeTruthy();
+            expect(body.message).toBe('deskId must be number');
+            done();
+          });
+      });
+      test('should be route exists', (done) => {
+        request(app)
+          .get('/queue/occupancy/' + 1)
+          .end((err, res) => {
+            expect(res.status).not.toBe(404);
+            done();
+          });
+      });
+
+      test('should get current occupancy', (done) => {
+        request(app)
+          .get('/queue/occupancy/' + 1)
+          .expect(200)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.total).toBe(3);
+            done();
+          });
+      });
+    });
+
+    
+    describe('Info', () => {
+      let queueNumber, normalDeskID, userID;
+      beforeEach(async () => {
+        /* 
+          Last Average Time 5
+          7 User (1 Finished, 1 deActivated) Front 
+          3 User (1 Finished) Back
+          Total : 7
+          Waiting Time : 25
+        */
+       userID = await new UserModel({
+        username: 'testxxxx',
+        password: '123456',
+        priority_key: 1,
+      })
+        .save()
+        .then((savedUser) => savedUser.id);
+        
+       normalDeskID = await new DeskModel({
+        name: 'test',
+        is_active: true,
+        is_deleted: false,
+      })
+        .save()
+        .then((savedDesk) => savedDesk.id);
+
+        await new AverageTimeModel({
+          desk_id: normalDeskID,
+          time: 10,
+        }).save();
+        await new AverageTimeModel({
+          desk_id: normalDeskID,
+          time: 5,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+          is_active: false,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+          is_finished: true,
+        }).save();
+        // Current User
+        queueNumber = await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        })
+          .save()
+          .then((savedQueue) => savedQueue.id);
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+          is_finished: true,
+        }).save();
+        await new DeskUserModel({
+          user_id: userID,
+          desk_id: normalDeskID,
+        }).save();
+      });
+      test('should queue number is number', (done) => {
+        request(app)
+          .get('/queue/info/asd')
+          .expect(400)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.error).toBeTruthy();
+            expect(body.message).toBe('queue number must be number');
+            done();
+          });
+      });
+      test('should queue number must be valid', (done) => {
+        request(app)
+          .get('/queue/info/9999')
+          .expect(400)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.error).toBeTruthy();
+            expect(body.message).toBe('queue number does not exists');
+            done();
+          });
+      });
+      test('should get correct info with queue number', (done) => {
+        request(app)
+          .get('/queue/info/' + queueNumber)
+          .expect(200)
+          .end((err, { body }) => {
+            expect(err).toBeNull();
+            expect(body.avgTime).toBe(5);
+            expect(body.frontCount).toBe(5);
+            expect(body.waitingTime).toBe(25);
+            done();
+          });
+      });
+    });
+  });
+});
