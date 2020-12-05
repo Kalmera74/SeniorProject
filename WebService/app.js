edf070b2237f46c4fb7cff286ff7b1311a760c9a
Erenalp0589 <64745658+Erenalp0589@users.noreply.github.com>
2020-12-05 18:52:51 +0300

    Add files via upload

--- WebService/app.js
+++ WebService/app.js
@@ -1,6 +1,7 @@
 import server from './server';
 import {program} from 'commander';
 
+//program details
 program
     .option('-m, --migrate', 'migrate database')
     .option('-s, --seed', 'insert seed data into database')
@@ -9,13 +10,15 @@ program
 
 program.parse(process.argv);
 
+
+//System working environment
 process.env.NODE_ENV = program.env || 'dev';
 
 console.info(`SET ENV : ${process.env.NODE_ENV}`);
 if (program.port) {
     console.info(`SET PORT : ${program.port}`);
 }
-
+// creation table
 if (program.migrate) {
     const db = require('./db/');
     db.migrate.rollback().then(() => {
--- WebService/server.js
+++ WebService/server.js
@@ -11,28 +11,37 @@ import portalRoutes from './routes/portal';
 import authMiddleware from './middleware/auth';
 import adminMiddleware from './middleware/admin';
 import portalMiddleware from './middleware/portal_user';
+import mobileMiddleware from './middleware/mobile_user';
 
 const pjson = require('./package.json');
 
 const app = express();
+
+//allow user to access to data to any origin 
 app.use(cors());
+
+//json data parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: 'application/json'}));
 
+//terminal log
 app.use(morgan('combined'));
 
-const apiPath = '/api/v' + pjson.version;
+// const apiPath = '/api/v' + pjson.version;
+const apiPath = '/api/'; 
 
 // API ROUTES
+
 app.use('/', authRoutes);
-app.use(apiPath, authMiddleware, userRoutes);
-app.use(apiPath + '/portal', authMiddleware, portalMiddleware, portalRoutes);
+
+app.use(apiPath +'mobile', authMiddleware, mobileMiddleware, userRoutes);
+app.use(apiPath +'portal', authMiddleware, portalMiddleware, portalRoutes);
 app.use(apiPath, authMiddleware, adminMiddleware, adminRoutes);
 
 const connect = (port) => {
-    port = port || 5001;
+    port = port || 5000;
 
     app.listen(port, () => {
         // qr_code_service.generateQR().then(qr_code_service.sendQR);
