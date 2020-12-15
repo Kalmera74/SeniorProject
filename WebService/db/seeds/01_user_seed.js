41e19d5c7003972145c6da0ec608dcad14689a2f
sbilgecaneryilmaz <48442657+SefIrmak@users.noreply.github.com>
2020-12-15 14:06:53 +0300

    Update 01_user_seed.js

--- WebService/db/seeds/01_user_seed.js
+++ WebService/db/seeds/01_user_seed.js
@@ -10,7 +10,7 @@ exports.seed = (knex) =>
                 priority_key: 3,
             },
             {
-                nationID: 12312,
+                nationID: 12345678999,
                 password: '123456',
                 priority_key: 0,
             },
