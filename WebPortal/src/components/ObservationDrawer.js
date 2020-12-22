6969d34be19065d977b731a98e842421462a2c79
Ibrahim Farajzade <45317395+ibrahimfarajzade@users.noreply.github.com>
2020-12-22 23:02:11 +0300

    web-portal

--- WebPortal/src/components/ObservationDrawer.js
+++ WebPortal/src/components/ObservationDrawer.js
@@ -12,15 +12,6 @@ const keyGen = () => {
   return r;
 };
 
-// to match all kinds of values, see https://www.hl7.org/fhir/observation.html
-const findValueKey = observation => {
-  let key,
-    keys = [];
-  let filter = new RegExp("value.*|component", "g");
-  for (key in observation) if (observation.hasOwnProperty(key) && filter.test(key)) keys.push(key);
-  return keys[0];
-};
-
 class ObservationDrawer extends React.Component {
   constructor(props) {
     super(props);
@@ -76,78 +67,6 @@ class ObservationDrawer extends React.Component {
   render() {
     const { visible } = this.props;
     const user = this.props.user && this.props.user.resource;
-
-    
-    const ViewRawBtn = props => {
-      return (
-        <div style={{ margin: "auto", textAlign: "center", padding: "10px 0" }}>
-          <a
-            onClick={() => {
-              this.openRawDataDrawer(props.object);
-            }}
-            disabled={this.props.loading}
-          >
-            View Raw FHIR Data
-          </a>
-        </div>
-      );
-    };
-
-    let observations =
-      this.state.observation &&
-      this.state.observation.map(entry => {
-        let obs = entry.resource;
-        let valueKey = findValueKey(obs);
-        let valueItems;
-        if (valueKey) {
-          valueItems = Object.keys(obs[valueKey]).map(key => {
-            if (key === "coding" || key === "system" || key === "code") return;
-            const value = obs[valueKey][key] + "";
-            return (
-              <Descriptions.Item key={keyGen()} label={key}>
-                {value}
-              </Descriptions.Item>
-            );
-          });
-
-          if (valueKey === "component") {
-            valueItems = obs.component.map(blood => {
-              return (
-                <Descriptions.Item key={keyGen()} label={blood.code?.text}>
-                  {blood.valueQuantity?.value + " " + blood.valueQuantity?.unit}
-                </Descriptions.Item>
-              );
-            });
-          }
-        }
-        return (
-          <div key={keyGen()} style={{ wordBreak: "break-all" }}>
-            <Descriptions
-              bordered={true}
-              layout={this.context.isMobile ? "horizontal" : "vertical"}
-              key={keyGen()}
-              title={obs.code.text}
-              column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
-            >
-              <Descriptions.Item key={keyGen()} label="ID">
-                {obs.id}
-              </Descriptions.Item>
-              {valueItems}
-              <Descriptions.Item key={keyGen()} label="Category">
-                {obs.category?.[0]?.coding?.[0].display}
-              </Descriptions.Item>
-              <Descriptions.Item key={keyGen()} label="issued">
-                {obs.issued}
-              </Descriptions.Item>
-              <Descriptions.Item key={keyGen()} label="effectiveDateTime">
-                {obs.effectiveDateTime}
-              </Descriptions.Item>
-            </Descriptions>
-
-            <ViewRawBtn object={obs}></ViewRawBtn>
-          </div>
-        );
-      });
 
     return (
       <Drawer
