3f0c1fe20e4d9c36aa3b6f83f15e8da23d80e7c5
kalmera74 <kalmera74@gmail.com>
2021-01-11 13:41:06 +0300

    fixes

--- Kiosk/main.py
+++ Kiosk/main.py
@@ -1,28 +1,106 @@
 from flask import Flask, request, jsonify
 import pyqrcode
-#import smbus2
 import os
+import json
+import requests
 
-
+token = ''
 
 app = Flask(__name__)
 
-queue = list()
+queue = []
 
-# Google Cloud Compute 
-host="10.128.0.3"
-port=80
+# Google Cloud Compute
+host = "168.119.190.83"
+port = 80
+
+Desks = list()
 
 
-# Uncomment when on Raspberry PI
-#bus = smbus.SMBus(1)
 
-DESK_ADDRESS_1 = 0x04
-DESK_ADDRESS_2 = 0x05
+
+def updateQueue(newQNumber):
+    if (newQNumber not in queue):
+        queue.append(newQNumber)
+        return True
+    return False
+
+
+def displayQR(qrnum):
+    if (int(qrnum) < 1 or qrnum == None):
+        return False
+    try:
+        url = pyqrcode.create(qrnum)
+        print(url.terminal(quiet_zone=1))
+        print(queue)
+        return True
+    except:
+        return False
+
+
+
+def authenticateAdmin():
+
+    resp = requests.post("https://senior.fastntech.com/auth/systemLogin",
+                         data={
+                             "username": "admin",
+                             "password": "admin"
+                         }
+                     )
+
+    global token                         
+    token = str(resp.content).replace("b'", '')
+    token = token.split(",")[0].split(":")[1]
+    
+
+
+authenticateAdmin()
+
+def authenticateMobile():
+    resp = requests.post("https://senior.fastntech.com/auth/mobileLogin", data={
+        "nationID":"12312",
+        "password":"123456"
+        }
+    )
+
+    token = str(resp.content).replace("b'",'')
+    token = token.split(",")[0].split(":")[1]
+
+    return token
+
+
+def getFirstQue():
+    token = authenticateMobile()
+    token = token.replace('"','')
+    headers = {"Authentication":token}
+    resp = requests.post("https://senior.fastntech.com/api/mobile/qr",headers=headers)
+    qnum = str(resp.content).split(":")[1]
+    qnum = qnum.split(",")[0]
+    updateQueue(int(qnum.replace('"','')))
+    displayQR(int(qnum.replace('"','')))
+
+
+
+getFirstQue()
+
+
+@app.route('/remove',methods=['POST'])
+def remove():
+ qnum = request.json["quenum"]
+ print(qnum)
+ que.remove(qnum);
+
+
+@app.route('/updatedesks', methods=['POST'])
+def updatedesks():
+    file = open("desklist.txt", "w")
+    file.write(request.json)
+
 
 @app.route("/iteratequeue", methods=["GET"])
 def iterate():
-    return jsonify({"qnumber":1,"address":0x05})
+    return jsonify({"qnumber": queue.pop(0)})
+
 
 @app.route("/", methods=["POST"])
 def detail():
@@ -39,28 +117,5 @@ def detail():
     return jsonify({"response": False})
 
 
-def updateQueue(newQNumber):
-    if (newQNumber not in queue):
-        queue.append(newQNumber)
-        return True
-    return False
-
-
-def displayQR(qrnum):
-    if (int(qrnum) < 1 or qrnum == None):
-        return False
-    try:
-        url = pyqrcode.create(qrnum)        
-        print(url.terminal(quiet_zone=1))
-        print(queue)
-        return True
-    except:
-        return False
-
-
-
-
-#app.run(host=host,port=port)
-app.run(debug=False,port=4000)
-
-
+app.run(host=host,port=port)
+#app.run(debug=False, port=80)
